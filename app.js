89c6427ad8276efcc71f7db97fba6e0256f48a12
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-07-25 13:01:44 -0300

    add diceGame in new git repository

---
+++ app.js
@@ -0,0 +1,26 @@
+let randomNumber1 = Math.floor( Math.random() * 6 ) + 1
+
+let randomDiceImage1 = 'img/dice'  + randomNumber1 + '.png'
+
+let image1 = document.querySelectorAll('img')[0]
+
+image1.setAttribute('src', randomDiceImage1)
+
+
+
+let randomNumber2 = Math.floor( Math.random() * 6 ) + 1
+
+let randomDiceImage2 = 'img/dice'  + randomNumber2 + '.png'
+
+let image2 = document.querySelectorAll('img')[1]
+
+image2.setAttribute('src', randomDiceImage2)
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector('h1').innerHTML = "🏆Vitória do Jogador 1🥇"
+}if(randomNumber1 < randomNumber2){
+    document.querySelector('h1').innerHTML = "🏆Vitória do Jogador 2🥇"
+}if(randomNumber1 == randomNumber2){
+    document.querySelector('h1').innerHTML = "Empate! Jogue🎲novamente"
+}
+
